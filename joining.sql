ec597134a8ef045211da3d5d6ffa4ea7956375ec
Yamini Khatri <43261810+khatry23@users.noreply.github.com>
2020-05-06 15:56:58 -0400

    Update joining.sql
    remove limit

--- joining.sql
+++ joining.sql
@@ -2,5 +2,5 @@ select n_killed, n_injured, offender_count, bias_desc
 from dbapp.gun_violence g , dbapp.hatecrimes h
 where g.state_name = h.state_name
 and hyear = DATE_PART_YEAR(incident_date)
-limit 5
+
 ;
